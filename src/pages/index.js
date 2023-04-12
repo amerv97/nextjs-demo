01f9a536e93d4d547950bed200f588581fdf6582
Amer Varajic <varajicamer@gmail.com>
2023-04-12 14:24:57 +0200

    Linking Between Pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,22 @@
 // our-domain.com
 
+import Link from "next/link";
+import { Fragment } from "react";
+
 function HomePage() {
-  return <h1>The Home Page</h1>;
+  return (
+    <Fragment>
+      <h1>The Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-great-framework">
+            NextJS is a great Framework
+          </Link>
+        </li>
+        <li>Something else..</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default HomePage;
